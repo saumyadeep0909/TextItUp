657afcfdda69549b81ab32da2e36c35326131af3
Saumyadeep Das <saumyadeep0909@gmail.com>
2025-07-20 18:22:34 +0530

    Fix: import error

--- messenger_project/messenger_project/asgi.py
+++ messenger_project/messenger_project/asgi.py
@@ -6,16 +6,19 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
-
 import os
-import django
+import sys
+
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import messenger.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger_project.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
