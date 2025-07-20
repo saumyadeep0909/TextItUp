ac4cc2ac0f9abee9cfb345f15967fee6912fd371
Saumyadeep Das <saumyadeep0909@gmail.com>
2025-07-20 13:39:54 +0530

    Set up ASGI, Redis via Upstash< and Render Deployment configs

--- messenger_project/messenger_project/asgi.py
+++ messenger_project/messenger_project/asgi.py
@@ -12,6 +12,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import messenger.routing
 from django.core.asgi import get_asgi_application
+import messenger.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger_project.settings')
 
--- messenger_project/messenger_project/settings.py
+++ messenger_project/messenger_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -140,7 +140,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [os.getenv("REDIS_URL")],
         },
     },
 }
