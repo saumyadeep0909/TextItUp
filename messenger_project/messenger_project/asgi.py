0c6bcee9118400fa44a08343c553f5b6b06fc0ac
Saumyadeep Das <saumyadeep0909@gmail.com>
2025-07-20 17:40:30 +0530

    fixed asgi.py

--- messenger_project/messenger_project/asgi.py
+++ messenger_project/messenger_project/asgi.py
@@ -10,17 +10,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import messenger.routing
+from messenger_project import routing
 from django.core.asgi import get_asgi_application
-import messenger.routing
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger_project.settings')
 
 application = ProtocolTypeRouter({
     "http" : get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            messenger.routing.websocket_urlpatterns
+            routing.websocket_urlpatterns
         )
     ),
 })
