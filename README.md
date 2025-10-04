4b879f8421fbea2bdbe7707920ca105455287f8e
Saumyadeep Das <saumyadeep0909@gmail.com>
2025-10-04 22:24:35 +0530

    Switch from SQLite to PostgreSQL

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#TextItUp 
+# TextItUp 
 
 Visit: https://textitup.onrender.com/login/
 
--- messenger_project/messenger_project/settings.py
+++ messenger_project/messenger_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,10 +81,7 @@ WSGI_APPLICATION = 'messenger_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
