dad31436c4ef3072c703a2a9cc7fac1716b59280
Saumyadeep Das <saumyadeep0909@gmail.com>
2025-11-05 02:35:43 +0530

    Fix database SSL connection isssue

--- messenger_project/messenger_project/settings.py
+++ messenger_project/messenger_project/settings.py
@@ -81,7 +81,11 @@ WSGI_APPLICATION = 'messenger_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
