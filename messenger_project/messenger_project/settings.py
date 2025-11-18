ce4d645c46823f9590e12f81e97f8302d710dfaf
Saumyadeep Das <saumyadeep0909@gmail.com>
2025-11-18 21:22:45 +0530

    updated ping

--- messenger_project/messenger_project/settings.py
+++ messenger_project/messenger_project/settings.py
@@ -149,7 +149,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.getenv("REDIS_URL")],
+            "hosts": [os.environ.get("REDIS_URL")],
         },
     },
 }
